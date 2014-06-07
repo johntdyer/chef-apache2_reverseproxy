66619f1908f332a04732cae9f78a667c0ac51d3d
Nick Santamaria <nick.santamaria@technocrat.com.au>
2014-06-07 21:33:09 +1000

    Initial commit of apache2_reverseproxy stub cookbook.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of apache2_reverseproxy
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: apache2_reverseproxy
+# Recipe:: default
+#
+# Copyright (C) 2014 Copyright Dropkey Pty Ltd
+#
+# All rights reserved - Do Not Redistribute
+#
